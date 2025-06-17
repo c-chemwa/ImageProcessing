131b684c76fdb1782712abde4ff47bef50604306
c-chemwa <calebchemwa@gmail.com>
2025-06-17 11:53:51 +0300

    image processing in python

---
+++ imageconversion_to_greyscale.py
@@ -0,0 +1,17 @@
+##TRANSFORM IMAGE TO GRAY SCALE
+from PIL import Image
+
+# Open an image file
+image = Image.open('/assets/mashle.png')
+
+# Display the image
+image.show()
+
+# Convert the image to grayscale
+gray_image = image.convert('L')
+
+# Display the grayscale image
+gray_image.show()
+
+# Save the grayscale image if needed
+gray_image.save('/assets/mashle_gray.png')
---
+++ imagedisplay.py
@@ -0,0 +1,10 @@
+##Basic Image Operations with Pillow
+##Opening and Displaying an Image
+from PIL import Image
+
+# Open an image file
+image = Image.open('assets/mashle.png')
+
+# Display the image
+image.show()
+
